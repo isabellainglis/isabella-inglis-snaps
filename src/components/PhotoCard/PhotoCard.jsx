8b91a4d0694d989782b854033382cfa1a766a0ff
Isabella Inglis <issy-inglis@hotmail.co.uk>
2025-02-25 17:39:48 +0000

    add photocard styling

--- src/components/PhotoCard/PhotoCard.jsx
+++ src/components/PhotoCard/PhotoCard.jsx
@@ -5,8 +5,14 @@ export default function PhotoCard() {
   return (
     <div className="photo-card">
       <div className="photo-card__img-container">
-        <img src="" alt="" className="photo-card__img" />
+        <img
+          src="https://unit-3-project-c5faaab51857.herokuapp.com/photos/Photo-00.png"
+          alt=""
+          className="photo-card__img"
+        />
         <div className="photo-card__footer">
+          <Tag />
+          <Tag />
           <Tag />
         </div>
       </div>

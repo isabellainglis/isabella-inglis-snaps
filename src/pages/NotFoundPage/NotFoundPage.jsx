4125c1b42e29d2ae9a3563fcb5e4e7edb95d7654
Isabella Inglis <issy-inglis@hotmail.co.uk>
2025-03-09 11:43:46 +0000

    add styling to 404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,11 @@
 import "./NotFoundPage.scss";
+import missingImage from "../../assets/images/question.jpg";
 
 export default function NotFoundPage() {
-  return <div>404 Not Found</div>;
+  return (
+    <div className="error">
+      <h1 className="error__heading">Oops</h1>
+      <h2 className="error__message">404 Page Not Found</h2>
+    </div>
+  );
 }
